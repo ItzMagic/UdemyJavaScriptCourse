c90900cc9f0487224b8a9f38a9ca752908a9ddea
Evan Londono <esodemoneyes@gmail.com>
2019-01-17 11:21:29 -0500

    started the course and learing 3 new methods which are include, startWith & endsWith

---
+++ stringMethods.js
@@ -0,0 +1,25 @@
+/* 
+
+include method - returns true if the given text is 
+ found anywhere within the string and returns false if not
+
+ startsWith method - returns trueif the text is found at the beginning is found
+    at the begining of the string and if not returns false 
+
+ endsWith method - returns true if the given string is found at the end of the text
+    if not it returns false.
+
+*Now each of these method accepts to arguments the text to search for and an optional
+ index from which to start the search.
+ 
+ The second argument minimizes the amount of the string being searched so if you use this
+ to optimize your approach.
+
+*/
+
+let msg = "Hello World!";
+
+console.log(msg.startsWith("Hello"));
+console.log(msh.endsWith("!"));
+console.log(msg.includes("o"));
+
